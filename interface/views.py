450727f293cff39b2b1ece79f0245b9cda41a490
osama <osamafitiani2001@gmail.com>
2025-02-22 15:29:14 +0300

    bla bla

--- interface/views.py
+++ interface/views.py
@@ -1,10 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.views.decorators.csrf import csrf_exempt
 from interface.utils import answer_query
 
 
+@csrf_exempt
 @api_view(['POST'])
 def ask(request):
     # Check if the request body contains the expected data
