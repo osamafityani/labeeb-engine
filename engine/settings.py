1fe67e46f6559b555270c3eefd93542f85756cfe
osama <osamafitiani2001@gmail.com>
2025-06-14 23:14:54 +0300

    destroy everything 3

--- engine/settings.py
+++ engine/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
