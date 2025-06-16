9166c4baefd143555d0410753b742ab68f94dd40
osama <osamafitiani2001@gmail.com>
2025-06-16 18:20:27 +0300

    rebuild everything 2

--- calendar_integration/urls.py
+++ calendar_integration/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'calendar_integration'
+
 urlpatterns = [
     path('auth-url/', views.get_auth_url, name='get_auth_url'),
     path('callback/', views.handle_callback, name='handle_callback'),
