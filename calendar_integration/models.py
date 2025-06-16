256848a5d2f42274beda4c02213772b7c7ecf71c
osama <osamafitiani2001@gmail.com>
2025-06-16 19:06:35 +0300

    rebuild everything 2

--- calendar_integration/models.py
+++ calendar_integration/models.py
@@ -5,9 +5,9 @@ User = get_user_model()
 
 class CalendarConnection(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    microsoft_token = models.JSONField()  # Store the OAuth tokens
-    refresh_token = models.CharField(max_length=500)
-    token_expiry = models.DateTimeField()
+    microsoft_token = models.JSONField(null=True, blank=True)  # Store the OAuth tokens
+    refresh_token = models.CharField(max_length=500, null=True, blank=True)
+    token_expiry = models.DateTimeField(null=True, blank=True)
     flow = models.JSONField(null=True, blank=True)  # Store the OAuth flow
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
