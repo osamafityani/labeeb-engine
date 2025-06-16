f2e9ddc6e7e130c08feb0f7ceb5035772ec1ca12
osama <osamafitiani2001@gmail.com>
2025-06-16 20:53:31 +0300

    rebuild everything 3

--- calendar_integration/o365_token_backend.py
+++ calendar_integration/o365_token_backend.py
@@ -7,22 +7,29 @@ class DjangoTokenBackend(BaseTokenBackend):
         super().__init__()
         self.user = user
 
-    def get_token(self):
+    def load_token(self) -> bool:
         try:
             token_obj = O365Token.objects.get(user=self.user)
-            return token_obj.token
+            self._cache = self.deserialize(token_obj.token)
+            return True
         except O365Token.DoesNotExist:
-            return None
+            self._cache = {}
+            return False
 
-    def save_token(self, token):
-        obj, created = O365Token.objects.update_or_create(
+    def save_token(self, force=False) -> bool:
+        if not self._has_state_changed and not force:
+            return True
+
+        serialized = self.serialize()
+        O365Token.objects.update_or_create(
             user=self.user,
             defaults={
-                'token': token,
+                'token': serialized,
                 'last_updated': timezone.now()
             }
         )
         return True
 
-    def delete_token(self):
-        O365Token.objects.filter(user=self.user).delete()
+    def delete_token(self) -> bool:
+        deleted, _ = O365Token.objects.filter(user=self.user).delete()
+        return deleted > 0
