7a05a18bc3a3bdc094500da951c9802138bf7921
osama <osamafitiani2001@gmail.com>
2025-06-16 19:09:31 +0300

    rebuild everything 2

--- calendar_integration/admin.py
+++ calendar_integration/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import CalendarConnection
 
-# Register your models here.
+@admin.register(CalendarConnection)
+class CalendarConnectionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'token_expiry', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('user__email', 'user__username')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user',)
+        }),
+        ('OAuth Information', {
+            'fields': ('microsoft_token', 'refresh_token', 'token_expiry', 'flow')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
